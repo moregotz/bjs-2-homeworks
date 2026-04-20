ad7ecc39d289791f9b9b7725445be97b38fab393
Moregotz <more.gotz@gmail.com>
2026-04-20 21:33:17 +0300

    task 2-1

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,4 +1,25 @@
 function getArrayParams(...arr) {
+  let min = Infinity;
+  let max = -Infinity;
+  let sum = 0;
+
+  for(let i = 0; i < arr.length; i++) {
+
+    let compare = arr[i];
+
+    if(compare > max){
+      max = compare;
+    }
+
+      if(compare < min){
+      min = compare;
+    }
+
+      sum += compare;
+    }
+
+  avg = sum / arr.length;
+  avg = Number(avg.toFixed(2));
 
   return { min: min, max: max, avg: avg };
 }
