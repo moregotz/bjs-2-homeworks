ae443e87518a9b789b5d85d0bf3f8af20dd5c3f6
Moregotz <more.gotz@gmail.com>
2026-04-20 22:06:05 +0300

    task 2-2

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -25,11 +25,38 @@ function getArrayParams(...arr) {
 }
 
 function summElementsWorker(...arr) {
+  let elementsSum = 0;
 
+  for(let i = 0; i < arr.length; i++) {
+    let compare = arr[i];
+    elementsSum += compare;
+    }
+
+    return elementsSum;
 }
 
 function differenceMaxMinWorker(...arr) {
+  let min = Infinity;
+  let max = -Infinity;
 
+  for(let i = 0; i < arr.length; i++) {
+
+    let compare = arr[i];
+
+    if(compare > max){
+      max = compare;
+    }
+
+      if(compare < min){
+      min = compare;
+    }
+  }
+
+  let differenceMaxMin = max - min;
+  
+  if (arr.length === 0) return 0;
+
+  return differenceMaxMin;
 }
 
 function differenceEvenOddWorker(...arr) {
